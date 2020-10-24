cce538fdd8b8c376ac73e32906c14e22ab4f4ddf
= <liam.percy@roguetwo.tech>
2020-10-25 02:05:16 +1000

    Adding build because EC2 can't do it

---
+++ client/build/precache-manifest.9479f6f3ac5e7bd0093a8b6f38e25b2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa85daa07c8895cc00b23d6e1bf09af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24917a7bb79e4561f42d",
+    "url": "/static/css/2.65dfc676.chunk.css"
+  },
+  {
+    "revision": "bac8cec42dbf7edb209f",
+    "url": "/static/css/main.139b1884.chunk.css"
+  },
+  {
+    "revision": "24917a7bb79e4561f42d",
+    "url": "/static/js/2.0d2d6776.chunk.js"
+  },
+  {
+    "revision": "9ea4d0ff9589015075626570b901b4b4",
+    "url": "/static/js/2.0d2d6776.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bac8cec42dbf7edb209f",
+    "url": "/static/js/main.acb50eb6.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
